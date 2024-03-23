55dee09f73a39f48622567a4c8743df5ad3fc694
eugene <glebajk@gmail.com>
2024-03-24 01:14:44 +0300

    Fixed cygwin path adding

--- scripts/install_apps.ps1
+++ scripts/install_apps.ps1
@@ -1,26 +1,6 @@
 #Requires -RunAsAdministrator
 
-Set-ExecutionPolicy Unrestricted
-
-function Enable-WindowsSandbox-And-WSL {
-    # Check if virtualization is enabled
-    $vmProcessor = Get-WmiObject -Class Win32_Processor | Select-Object -First 1
-    if ($vmProcessor.SecondLevelAddressTranslationExtensions -notcontains "True") {
-        Write-Warning "Virtualization is not enabled. Please enable virtualization in the BIOS and try again."
-        return
-    }
-
-    # Enable Windows Sandbox
-    Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online -NoRestart
-
-    # Check if WSL is installed and install it if not
-    if (!(Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -eq 'Enabled') {
-        Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-    }
-}
-
-Enable-WindowsSandbox-And-WSL
-
+# Set-ExecutionPolicy Unrestricted
 
 # Function to install packages via Chocolatey
 function Install-Packages {
@@ -72,6 +52,7 @@ $packages = @(
     'vlc',
     'nilesoft-shell',
     'steam-client',
+    'spotify'
     'vscodium',
     'gpg4win'
     'jetbrainstoolbox',
@@ -129,8 +110,15 @@ Install-Packages -Packages $packages
 choco upgrade all -y | Out-File -FilePath "$env:USERPROFILE\windows_automated_apps_setup.log" -Append
 
 # Adding UNIX-like cmd utils (e.g. `ls`, `pwd` and etc.)
-$env:Path += ";C:\tools\cygwin\bin"
+$folderPath += ";C:\tools\cygwin\bin"
 
-Set-ExecutionPolicy Restricted
+# Get the current PATH variable
+$currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
+
+# Append the new folder to the PATH
+$newPath = $currentPath + ";" + $folderPath
+
+# Set the new PATH variable
+[Environment]::SetEnvironmentVariable("Path", $newPath, "User")
 
 Pause
